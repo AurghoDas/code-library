6a200d9b385713c7023c5bcaee9873e1f4509de8
Prahar Das Aurgho <aurgho.me@gmail.com>
2025-05-29 16:00:03 +0600

    Create 1.cpp

---
+++ bitwise operation/1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 4;//4 = 100
+    int b = 6;//6 = 110
+
+    cout << "a&b = " << (a&b) << endl;
+    cout << "a|b = " << (a|b) << endl;
+    cout << "~a = " << (~a) << endl;
+    cout << "a^b = " << (a^b) << endl;
+
+    cout << (17>>1) << endl;
+    cout << (17>>2) << endl;
+    cout << (19<<1) << endl;
+    cout << (21<<2) << endl;
+} 
