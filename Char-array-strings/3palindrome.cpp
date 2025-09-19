21adb09bee710e3e16bd2ca90fd3a0d4abe8735b
Prahar Das Aurgho <aurgho.me@gmail.com>
2025-09-19 18:08:21 +0600

    Create 3palindrome.cpp

---
+++ Char-array-strings/3palindrome.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll         long long
+#define dl         long double
+#define endl       '\n'
+#define fastio     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define gcd(a,b)     __gcd(a,b)
+#define lcm(a,b)   (a*b)/__gcd(a,b)
+#define w(x)       int x; cin >> x; while(x--)
+
+
+bool ispalindrome(char str[], int n) {
+    int st=0, end=n-1;
+    while(st < end) {
+        if(str[st++] != str[end--]) {
+            cout << "Not Palindrome";
+            return false;
+        }
+    }
+    cout << "Palindrome";
+    return true;
+}
+
+int main() {
+    fastio;
+    
+    char word[] = "abba";
+    ispalindrome(word, strlen(word));
+
+    return 0;
+}
