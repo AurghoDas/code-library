850213b6c81a6e155b3ec13bb44bb1ca917d6e44
Prahar Das Aurgho <aurgho.me@gmail.com>
2025-09-22 23:36:45 +0600

    Create 4update_ith_bit.cpp

---
+++ bitwise operation/4update_ith_bit.cpp
@@ -0,0 +1,27 @@
+//write a function to update thr ith bit in a number
+//according to given value(0 & 1)
+
+#pragma GCC optimize("O3,unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll         long long
+#define dl         long double
+#define endl       '\n'
+#define fastio     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define gcd(a,b)     __gcd(a,b)
+#define lcm(a,b)   (a*b)/__gcd(a,b)
+#define w(x)       int x; cin >> x; while(x--)
+
+int updateIthBit(int num, int i) {
+    int bitMask = 1 << i;
+    return (num | bitMask);
+}
+
+int main() {
+    fastio;
+    
+    cout << updateIthBit(6, 3) << endl;
+
+    return 0;
+}
